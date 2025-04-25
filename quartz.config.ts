ed09ee5d5547aac24ca12040fb2f9936c6ce5a17
huseyn.zeynalov <huseyn.zeynalov@gr8.tech>
2025-04-25 02:06:46 +0200

    Quartz sync: Apr 25, 2025, 2:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "doincreative.com",
+    baseUrl: "/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
